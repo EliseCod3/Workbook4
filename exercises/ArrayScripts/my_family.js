96cc5996a67b2085e8506653cdc4eed9445ce607
elise bell <eliseraynahasawsomesauce@gmail.com>
2022-10-11 16:26:22 -0400

    Started exercise 2

---
+++ exercises/ArrayScripts/my_family.js
@@ -0,0 +1,8 @@
+let famNames = ["Ralph", "Jackie", "Jeraud", "Justin"];
+let familyLength = famNames.length;
+let count = 1;
+
+for (let count = 0; count < famNames.length; count++) {
+    const element = famNames[count];
+    console.log(element);
+}
