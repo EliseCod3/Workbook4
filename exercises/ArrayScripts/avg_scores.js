aac8ef104024384aab996c0f33b1e5aca92f9470
elise bell <eliseraynahasawsomesauce@gmail.com>
2022-10-12 10:05:34 -0400

    finished exercise

--- exercises/ArrayScripts/avg_scores.js
+++ exercises/ArrayScripts/avg_scores.js
@@ -1,4 +1,15 @@
 let myScores = [92, 98, 84, 76, 89, 99, 100];
 let yourScores = [82, 98, 94, 88, 92, 100, 100];
 
+function getAverage(scores) {
+    let total = 0;
+    for (let count = 0; count < scores.length; count++) {
+        total = total + scores[count];
+    }
+    return total/scores.length;
+}
+let averageScore = getAverage(myScores);
+let secondAverage = getAverage(yourScores);
 
+console.log(averageScore.toFixed(2));
+console.log(secondAverage.toFixed(2));
