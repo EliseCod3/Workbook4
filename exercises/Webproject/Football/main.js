d1b6ffc1681580ca10aa74a5aaa18acdb39e14dd
elise bell <eliseraynahasawsomesauce@gmail.com>
2022-10-13 15:51:25 -0400

    Finished exercise one

---
+++ exercises/Webproject/Football/main.js
@@ -0,0 +1,45 @@
+"use strict";
+
+let teams = [
+    {code:"DAL", name:"Dallas Cowboys", plays:"Arlington, TX"},
+    {code:"DEN", name:"Denver Broncos", plays:"Denver, CO"},
+    {code:"HOU", name:"Houston Texans", plays:"Houston, TX"},
+    {code:"KAN", name:"Kansas City Chiefs",
+    plays:"Kansas City, MO"},
+];
+
+    window.onload = function () {
+        loadTeamsDropdown();
+    };
+    
+    function loadTeamsDropdown() {
+        const teamsList = document.getElementById("teamsList");
+        teams.sort();
+        teams.sort(function(a, b) {
+            if (a.name < b.name) return -1;
+            else if (a.name == b.name) return 0;
+            else return 1;
+        });
+        
+       for (const team of teams) {
+        let option = new Option(team.name, team.code);
+        teamsList.appendChild(option);
+       }
+        
+    }
+
+    function displayTeams() {
+        const teamsList = document.getElementById("teamsList");
+       const teamCode = teamsList.value;
+       for (let index = 0; index < teams.length; index++) {
+        const team = teams[index];
+        if (team.code == teamCode) {
+            console.log(`You selected ${team.name} (${team.code}) who play in ${team.plays}`);
+        }
+       }
+       }
+    
+
+    function handleChange() {
+        console.log("changed");
+    }
